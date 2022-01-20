679499cc9b92cfb0ae1dccbfd7e97ce719d23576
Jerome Gaillard <jgaillard@google.com>
2022-01-20 13:22:43 +0000

    Fix build of dng_sdk for macOS
    On mac, long and long long are of the same size, but int64_t is specifically defined as a long long.
    
    Bug: N/A
    Change-Id: Ib25cec45621c4eefc703de5f5e43b16dc0a010a3
    Test: on a mac, lunch sdk & m libdng_sdk
    (cherry picked from commit 1e1c945266845332b99d04a375729eee99826e22)
    Merged-In: Ib25cec45621c4eefc703de5f5e43b16dc0a010a3

--- source/dng_safe_arithmetic.h
+++ source/dng_safe_arithmetic.h
@@ -113,7 +113,7 @@ std::int64_t SafeInt64MultSlow(std::int64_t arg1, std::int64_t arg2);
 #if __has_builtin(__builtin_smull_overflow)
 inline std::int64_t SafeInt64MultByClang(std::int64_t arg1, std::int64_t arg2) {
   std::int64_t result;
-#if LONG_MAX == INT64_MAX
+#if (LONG_MAX == INT64_MAX) && !defined(__APPLE__)
   if (__builtin_smull_overflow(arg1, arg2, &result)) {
 #else
   if (__builtin_smulll_overflow(arg1, arg2, &result)) {
